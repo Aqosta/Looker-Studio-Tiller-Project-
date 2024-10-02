e22ceaf04adfb3c5a11c057ba90273c8fa5bdca6
Aqosta <aqosta7@gmail.com>
2024-10-02 15:30:52 +0200

    renamed the columns :dim_type1 & dim_status1

---
+++ renaming_columns.sql
@@ -0,0 +1,5 @@
+CREATE OR REPLACE TABLE `speedy-now-423709-s4.tiller.1513_orders_lines_payments` AS 
+SELECT
+  dim_status_1 AS pay_status,
+  dim_type_1 AS pay_type, 
+FROM `speedy-now-423709-s4.tiller.1513_orders_lines_payments` 
